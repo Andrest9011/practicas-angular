251d2e33d107cf39802024d1e2a99f42e04504ed
Andres <andrestellez2009@gmail.com>
2020-11-30 12:05:00 -0500

    Base structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+   {
+     provide: LocationStrategy,
+     useClass: PathLocationStrategy
+   }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
