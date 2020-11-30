cfd89ac08ab00160f56e717663194b4a04c9fd27
Andres <andrestellez2009@gmail.com>
2020-11-30 13:54:33 -0500

    Layout created and base styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SkeletonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
