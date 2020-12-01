e4f459615301117532e696ea3ad71e0dc00dfc0d
Andres <andrestellez2009@gmail.com>
2020-12-01 08:58:53 -0500

    First component user-list, created

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,9 @@
+import { TitleH1Component } from './title-h1/title-h1.component';
+
 
 export const components: any[] = [
-
+    TitleH1Component
 ];
+
+export * from './title-h1/title-h1.component';
 
